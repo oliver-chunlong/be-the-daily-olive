87015a45b3aa5f2b6fffe7d1d0ff58994274a191
Oliver <oliver@Davids-MacBook-Pro.local>
2025-04-15 21:57:20 +0100

    Inserting articles data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -25,9 +25,43 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
       );
     })
     .then(() => {
-      return db.query(
-        `INSERT INTO topics(slug, description, img_url) VALUES('mitch', 'The man, the Mitch, the legend', '')`
+      const formattedTopics = topicData.map((topic) => {
+        return [topic.description, topic.slug, topic.img_url];
+      });
+      const insertTopicsQuery = format(
+        `INSERT INTO topics(slug, description, img_url) VALUES %L`,
+        formattedTopics
       );
+      return db.query(insertTopicsQuery);
+    })
+    .then(() => {
+      const formattedUsers = userData.map((user) => {
+        return [user.username, user.name, user.avatar_url];
+      });
+      const insertUsersQuery = format(
+        `INSERT INTO users(username, name, avatar_url) VALUES %L`,
+        formattedUsers
+      );
+      return db.query(insertUsersQuery);
+    })
+    .then(() => {
+      const formattedArticles = articleData.map((article) => {
+        return [
+          article.article_id,
+          article.title,
+          article.topic,
+          article.author,
+          article.body,
+          article.created_at,
+          article.votes,
+          article.article_img_url,
+        ];
+      });
+      const insertArticlesQuery = format(
+        `INSERT INTO articles(article_id, title, topic, author, body, created_at, votes, article_img_url) VALUES %L`,
+        formattedArticles
+      );
+      return db.query(insertArticlesQuery);
     });
 };
 
