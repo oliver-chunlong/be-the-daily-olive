1495b55fc3639a6bb5b0ef56870db5b062e75fa9
Oliver <oliver@MacBookPro.fritz.box>
2025-04-28 20:55:11 +0100

    complete task 3

--- app/controllers/get-api.controller.js
+++ app/controllers/get-api.controller.js
@@ -31,7 +31,7 @@ exports.getArticleById = (req, res) => {
       } else if (err.status && err.msg) {
         return res.status(err.status).send({ msg: err.msg });
       } else {
-        return res.status(500).send({ msg: "Error: Internal Server" });
+        return res.status(500).send({ msg: "Internal Server Error" });
       }
     });
 };
