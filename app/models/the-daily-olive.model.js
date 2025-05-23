46cd870279793de3eb9347d6992dc3e42884c531
Oliver <oliver@MacBookPro.fritz.box>
2025-05-23 16:08:19 +0100

    add valid topics

--- app/models/the-daily-olive.model.js
+++ app/models/the-daily-olive.model.js
@@ -20,7 +20,7 @@ exports.selectArticleById = (article_id) => {
 exports.fetchArticles = (sort_by, order, topic) => {
   let queryStr = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id`;
 
-  const validTopics = ["mitch", "cats"];
+  const validTopics = ["mitch", "cats", "coding", "football", "cooking"];
 
   if (topic && validTopics.includes(topic)) {
     queryStr += ` WHERE topic = '${topic}' GROUP BY articles.article_id`;
